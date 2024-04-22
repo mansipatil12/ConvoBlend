5c3eaaf625ca56a52cc1e85d2e99563c94d23d63
mansi patil <patilmansi1201@gmail.com>
2024-04-22 19:06:30 +0530

    app.js update

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
 import './App.css';
 
-const projectID = 'de5c11b7-dac9-465f-afda-a6f5cc3d144e';
-
 const App = () => {
-
+  
   return (
     <ChatEngine
       height="100vh"
-      projectID={projectID}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
+      projectID="de5c11b7-dac9-465f-afda-a6f5cc3d144e"
+      userName="Mansi"
+      userSecret="1234"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
-
-// infinite scroll, logout, more customizations...
 
 export default App;
